83035f4ea4f276731fd1fc53e14a42ee3c54b873
darknessF0rd <106842169+darknessF0rd@users.noreply.github.com>
2022-10-20 20:45:10 -0500

    _ParticlesAnadidas

--- Assets/_Game/Scripts/InputSystem/PlayerInputController.cs
+++ Assets/_Game/Scripts/InputSystem/PlayerInputController.cs
@@ -90,8 +90,7 @@ public class PlayerInputController : MonoBehaviour
 
     public void MovementPerformed(InputAction.CallbackContext ctx)
     {
-        GetComponent<MovementController>().MoveInputVector = ctx.ReadValue<Vector2>();
-        GetComponent<LockMovementController>().MoveInputVector = ctx.ReadValue<Vector2>();
+        GetComponent<MovementController>().MoveInputVector = ctx.ReadValue<Vector2>();        
     }
 
     public void JumpPerformed(InputAction.CallbackContext ctx)
