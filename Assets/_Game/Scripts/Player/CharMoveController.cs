99ef0987d95ee360b87ae14bdb37130a17707e23
Santiago Jaramillo Ocampo <jaramilloocamposantiago@gmail.com>
2022-10-23 21:11:31 -0500

    Updated scenes

--- Assets/_Game/Scripts/Player/CharMoveController.cs
+++ Assets/_Game/Scripts/Player/CharMoveController.cs
@@ -106,7 +106,7 @@ using UnityEngine;
             isGrounded = true;
             isJumping = false;
             isFalling = false;
-            if (Physics.CheckSphere(groundCheck.position, .02f, groundLayer) && playerVelocity.y < 0)
+            if (Physics.CheckSphere(groundCheck.position, .03f, groundLayer) && playerVelocity.y < 0)
             {
                 playerVelocity.y = 0f;
             }
