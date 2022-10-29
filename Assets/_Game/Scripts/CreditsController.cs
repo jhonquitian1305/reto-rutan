a68d0a87ba229a80c0699ef6250755645315835c
Santiago Jaramillo Ocampo <jaramilloocamposantiago@gmail.com>
2022-10-29 11:16:53 -0500

    fix creditos

--- Assets/_Game/Scripts/CreditsController.cs
+++ Assets/_Game/Scripts/CreditsController.cs
@@ -6,21 +6,24 @@ using UnityEngine.SceneManagement;
 public class CreditsController : MonoBehaviour
 {
     public AnimationClip _animation;
+    private SoundManager soundManager;
     public void Start()
     {
-        FindObjectOfType<SoundManager>().Play("Background" + SceneManager.GetActiveScene().buildIndex);
+        if (soundManager != null) soundManager.Play("Background" + SceneManager.GetActiveScene().buildIndex);
+        soundManager = FindObjectOfType<SoundManager>();
+
     }
     public void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
         {
-            FindObjectOfType<SoundManager>().Stop("Background" + SceneManager.GetActiveScene().buildIndex);
+            if(soundManager!=null) soundManager.Stop("Background" + SceneManager.GetActiveScene().buildIndex);
             SceneManager.LoadScene(0);  
         }
     }
     public void VolverEscenaAnterior()
     {
-        FindObjectOfType<SoundManager>().Stop("Background" + SceneManager.GetActiveScene().buildIndex);
+        if (soundManager != null) soundManager.Stop("Background" + SceneManager.GetActiveScene().buildIndex);
         SceneManager.LoadScene(0);
     }
 }
